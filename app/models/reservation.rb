21d3c152feecb780faf4adfc1c03a9a827014f7f
ryuryu5121 <r.oshima.12.15@gmail.com>
2023-05-18 14:44:03 +0900

    曜日データを取り出すsqlとログイン機能の修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,9 +1,9 @@
 class Reservation < ApplicationRecord
   def self.reservations_after_three_month
-    # 今日から3ヶ月先までのデータを取得
-    reservations = Reservation.all.where("day >= ?", Date.current).where("day < ?", Date.current >> 3).order(day: :desc)
-    # 配列を作成し、データを格納
-    # DBアクセスを減らすために必要なデータを配列にデータを突っ込んでます
+    # 今日から3ヶ月先までの予約情報（年月日、時間）を取得
+    # reservations = Reservation.all.where("day >= ?", Date.current).where("day < ?", Date.current >> 3).order(day: :desc)
+    reservations = Reservation.all.where(day: (Date.current)..(Date.current.since(3.month)))
+    # DBアクセスを減らすために必要なデータを配列に保存
     reservation_data = []
     reservations.each do |reservation|
       reservations_hash = {}
