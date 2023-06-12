60b9b2c201698f4e3b6e636908f71d9b3c3f2bb5
ryuryu5121 <r.oshima.12.15@gmail.com>
2023-06-12 15:41:24 +0900

    比較用データ

--- app/controllers/diagnostics_controller.rb
+++ app/controllers/diagnostics_controller.rb
@@ -6,7 +6,7 @@ class DiagnosticsController < ApplicationController
   end
 
   def diagnostic
-    @result = { years_old: params[:years_old] }
+    @result = { years_old: params[:years_old], family: params[:family], salary: params[:salary], rent: params[:rent], food_expenses: params[:food_expenses], utility_bills: params[:utility_bills], clothes: params[:clothes], medical_insurance: params[:medical_insurance], communication_cost: params[:communication_cost], education_cost: params[:education_cost], entertainment_cost: params[:entertainment], saving: params[:saving], anyother: params[:anyother]}
     redirect_to diagnostics_show_path(result: @result)
   end
 end
