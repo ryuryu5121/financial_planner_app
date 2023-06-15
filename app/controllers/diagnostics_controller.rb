9db79407ee0d1a0008f5d2e72831cc88109f8d87
ryuryu5121 <r.oshima.12.15@gmail.com>
2023-06-15 13:53:24 +0900

    データを渡す処理と年齢に応じたデータを渡す処理

--- app/controllers/diagnostics_controller.rb
+++ app/controllers/diagnostics_controller.rb
@@ -1,45 +1,45 @@
 class DiagnosticsController < ApplicationController
-  MONWY_DATA_20S = { rent: 40000, food_expenses: 54000, utility_bills: 17000, clothes: 9000, medical_insurance: 9000, communication_cost: 38000, education_cost: 2000, entertainment_cost: 19000, saving: 5, anyother: 43000}
+  MONEY_DATA_20S = { rent: 40000, food_expenses: 54000, utility_bills: 17000, clothes: 9000, medical_insurance: 9000, communication_cost: 38000, education_cost: 2000, entertainment_cost: 19000, saving: 5, anyother: 43000}
 
-  MONWY_DATA_30S = { rent: 24000, food_expenses: 73000, utility_bills: 21000, clothes: 11000, medical_insurance: 12000, communication_cost: 43000, education_cost: 8000, entertainment_cost: 29000, saving: 77, anyother: 40000}
+  MONEY_DATA_30S = { rent: 24000, food_expenses: 73000, utility_bills: 21000, clothes: 11000, medical_insurance: 12000, communication_cost: 43000, education_cost: 8000, entertainment_cost: 29000, saving: 77, anyother: 40000}
 
-  MONWY_DATA_40S = { rent: 17000, food_expenses: 84000, utility_bills: 24000, clothes: 13000, medical_insurance: 13000, communication_cost: 52000, education_cost: 26000, entertainment_cost: 33000, saving: 50, anyother: 52000}
+  MONEY_DATA_40S = { rent: 17000, food_expenses: 84000, utility_bills: 24000, clothes: 13000, medical_insurance: 13000, communication_cost: 52000, education_cost: 26000, entertainment_cost: 33000, saving: 50, anyother: 52000}
 
-  MONWY_DATA_50S = { rent: 19000, food_expenses: 84000, utility_bills: 26000, clothes: 14000, medical_insurance: 12000, communication_cost: 14000, education_cost: 29000, entertainment_cost: 30000, saving: 54, anyother: 75000}
+  MONEY_DATA_50S = { rent: 19000, food_expenses: 84000, utility_bills: 26000, clothes: 14000, medical_insurance: 12000, communication_cost: 14000, education_cost: 29000, entertainment_cost: 30000, saving: 54, anyother: 75000}
 
-  MONWY_DATA_60S = { rent: 21000, food_expenses: 81000, utility_bills: 26000, clothes: 9000, medical_insurance: 16000, communication_cost: 48000, education_cost: 4000, entertainment_cost: 26000, saving: 300, anyother: 66000}
+  MONEY_DATA_60S = { rent: 21000, food_expenses: 81000, utility_bills: 26000, clothes: 9000, medical_insurance: 16000, communication_cost: 48000, education_cost: 4000, entertainment_cost: 26000, saving: 300, anyother: 66000}
 
-  MONWY_DATA_OLDERS = { rent: 20000, food_expenses: 74000, utility_bills: 25000, clothes: 7000, medical_insurance: 15000, communication_cost: 32000, education_cost: 1000, entertainment_cost: 24000, saving: 1500, anyother: 51000}
+  MONEY_DATA_OLDERS = { rent: 20000, food_expenses: 74000, utility_bills: 25000, clothes: 7000, medical_insurance: 15000, communication_cost: 32000, education_cost: 1000, entertainment_cost: 24000, saving: 1500, anyother: 51000}
 
 
   def index
   end
 
   def show
-    @user_diagnostic = { years_old: params[:years_old], family: params[:family], salary: params[:salary], rent: params[:rent], food_expenses: params[:food_expenses], utility_bills: params[:utility_bills], clothes: params[:clothes], medical_insurance: params[:medical_insurance], communication_cost: params[:communication_cost], education_cost: params[:education_cost], entertainment_cost: params[:entertainment], saving: params[:saving], anyother: params[:anyother]}
-    years_old = params[:years_old]
-    @average = average(years_old)
-    @result = diagnostics_years_old(years_old, @user_diagnostic)
+    @user_diagnostic = { years_old: params[:result][:years_old].to_i, family: params[:result][:family].to_i, salary: params[:result][:salary].to_i, rent: params[:result][:rent].to_i, food_expenses: params[:result][:food_expenses].to_i, utility_bills: params[:result][:utility_bills].to_i, clothes: params[:result][:clothes].to_i, medical_insurance: params[:result][:medical_insurance].to_i, communication_cost: params[:result][:communication_cost].to_i, education_cost: params[:result][:education_cost].to_i, entertainment_cost: params[:result][:entertainment].to_i, saving: params[:result][:saving].to_i, anyother: params[:result][:anyother].to_i}
+    @years_old = params[:years_old].to_i
+    @average = average(@years_old)
+    @result = diagnostics_years_old(@years_old, @user_diagnostic)
   end
 
   def diagnostic
     @user_diagnostic = { years_old: params[:years_old], family: params[:family], salary: params[:salary], rent: params[:rent], food_expenses: params[:food_expenses], utility_bills: params[:utility_bills], clothes: params[:clothes], medical_insurance: params[:medical_insurance], communication_cost: params[:communication_cost], education_cost: params[:education_cost], entertainment_cost: params[:entertainment], saving: params[:saving], anyother: params[:anyother]}
-    redirect_to diagnostics_show_path(result: @result)
+    redirect_to diagnostics_show_path(result: @user_diagnostic)
   end
 
   def diagnostics_years_old(years_old, user_diagnostic)
     if years_old < 30
-      compare_data(user_diagnostic, MONWY_DATA_20S)
+      compare_data(user_diagnostic, MONEY_DATA_20S)
     elsif years_old < 40
-      compare_data(user_diagnostic, MONWY_DATA_30S)
+      compare_data(user_diagnostic, MONEY_DATA_30S)
     elsif years_old < 50
-      compare_data(user_diagnostic, MONWY_DATA_40S)
+      compare_data(user_diagnostic, MONEY_DATA_40S)
     elsif years_old < 60
-      compare_data(user_diagnostic, MONWY_DATA_50S)
+      compare_data(user_diagnostic, MONEY_DATA_50S)
     elsif years_old < 70
-      compare_data(user_diagnostic, MONWY_DATA_60S)
+      compare_data(user_diagnostic, MONEY_DATA_60S)
     else
-      compare_data(user_diagnostic, MONWY_DATA_OLDERS)
+      compare_data(user_diagnostic, MONEY_DATA_OLDERS)
     end
   end
 
@@ -49,17 +49,17 @@ class DiagnosticsController < ApplicationController
 
   def average(years_old)
     if years_old < 30
-      MONWY_DATA_20S
+      MONEY_DATA_20S
     elsif years_old < 40
-      MONWY_DATA_30S
+      MONEY_DATA_30S
     elsif years_old < 50
-      MONWY_DATA_40S
+      MONEY_DATA_40S
     elsif years_old < 60
-      MONWY_DATA_50S
+      MONEY_DATA_50S
     elsif years_old < 70
-      MONWY_DATA_60S
+      MONEY_DATA_60S
     else
-      MONWY_DATA_OLDERS
+      MONEY_DATA_OLDERS
     end
   end
 
