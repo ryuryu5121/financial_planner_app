8a52f1ec450c4806d7f60df554483c88af424053
ryuryu5121 <r.oshima.12.15@gmail.com>
2023-06-19 11:55:47 +0900

    結果のまとめを表示するように変更

--- app/controllers/diagnostics_controller.rb
+++ app/controllers/diagnostics_controller.rb
@@ -21,6 +21,7 @@ class DiagnosticsController < ApplicationController
     @average = average(@years_old)
     @result = diagnostics_years_old(@years_old, @user_diagnostic)
     @items = [ 'rent', 'food_expense', 'utility_bills', 'clothes', 'medical_insurance' , 'communication_cost', 'education_cost', 'entertainment_cost', 'saving', 'anyother' ]
+    @count = count_money_problems(@result)
   end
 
   def diagnostic
@@ -64,26 +65,15 @@ class DiagnosticsController < ApplicationController
     end
   end
 
-
-  money_data_20s = [40000, 54000, 17000, 9000, 9000, 38000, 2000, 19000, 5, 43000]
-
-  def money_data_30s
-    money_data_30s = [24000, 73000, 21000, 11000, 12000, 43000, 8000, 29000, 77, 40000]
-  end
-
-  def money_data_40s
-    money_data_40s = [17000, 84000, 24000, 13000, 13000, 52000, 26000, 33000, 50, 52000]
-  end
-
-  def money_data_50s
-    money_data_50s = [19000, 84000, 26000, 14000, 12000, 14000, 29000, 30000, 54, 75000]
-  end
-
-  def money_data_60s
-    money_data_60s = [21000, 81000, 26000, 9000, 16000, 48000, 4000, 26000, 300, 66000]
-  end
-
-  def money_data_olders
-    money_data_olders = [20000, 74000, 25000, 7000, 15000, 32000, 1000, 24000, 1500, 51000]
+  def count_money_problems(result)
+    count = 0
+    result.each do |key, value|
+      if key === :saving && value < 0
+        count += 1
+      elsif value > 0
+        count += 1
+      end
+    end
+    return count
   end
 end
