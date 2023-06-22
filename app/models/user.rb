e52d379b2380fd5bc1b69fb50009750792c60fdf
ryuryu5121 <r.oshima.12.15@gmail.com>
2023-06-22 10:47:44 +0900

    別のプルリクでの変更だったので取り消し

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,34 +18,4 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-
-  def User.new_token
-    SecureRandom.urlsafe_base64
-  end
-
-  def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
-    return false if digest.nil?
-    BCrypt::Password.new(digest).is_password?(token)
-  end
-
-  def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
-  end
-
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
-  end
-
-  private
-
-  def downcase_email
-    self.email = email.downcase
-  end
-
-  def create_activation_digest
-    self.activation_token  = User.new_token
-    self.activation_digest = User.digest(activation_token)
-  end
 end
